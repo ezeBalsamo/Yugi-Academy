c8b3176cb5185acdf6e5f8a019b43c6b8c1c9811
NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>
2022-09-04 09:34:21 -0300

    :construction: Add views used in urls path

--- source/YugiAcademy/YuGiOh/views.py
+++ source/YugiAcademy/YuGiOh/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def inicio(request):
+    return render(request, "YuGiOh/index2.html")
+
+
+def cards(request):
+    return render(request, "YuGiOh/cards.html")
