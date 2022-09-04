e9fe59a756222b9063ff2f11d51fe4971b49aa46
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-04 16:09:31 -0300

    :art: Applying PEP8 recommendations

--- source/YugiAcademy/YuGiOh/views.py
+++ source/YugiAcademy/YuGiOh/views.py
@@ -6,14 +6,18 @@ from django.shortcuts import render
 def home(request):
     return render(request, "YuGiOh/index.html")
 
+
 def cards(request):
     return render(request, "YuGiOh/cards.html")
+
 
 def decks(request):
     return render(request, "YuGiOh/decks.html")
 
+
 def about(request):
     return render(request, "YuGiOh/about.html")
+
 
 def login(request):
     return render(request, "YuGiOh/login.html")
