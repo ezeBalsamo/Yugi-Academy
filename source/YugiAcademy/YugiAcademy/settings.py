a2dca12d548f3a24b539bff4b22683e79d304dec
NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>
2022-09-04 09:36:26 -0300

    :construction: change in DIRS (path) for templates

--- source/YugiAcademy/YugiAcademy/settings.py
+++ source/YugiAcademy/YugiAcademy/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'YugiAcademy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(__file__).resolve().parent.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
