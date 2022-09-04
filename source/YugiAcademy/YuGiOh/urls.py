e1a3f8a1a9240613af0dae6bf6fb004680576321
NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>
2022-09-04 11:38:22 -0300

    :construction: add urls paths for myCards and aboutUs pages

--- source/YugiAcademy/YuGiOh/urls.py
+++ source/YugiAcademy/YuGiOh/urls.py
@@ -4,5 +4,7 @@ from YuGiOh import views
 urlpatterns = [
     path('', views.inicio, name="Inicio"),
     path('cards', views.cards, name="Cards"),
-    path('decks', views.decks, name="Decks")
+    path('decks', views.decks, name="Decks"),
+    path('myCards', views.myCards, name="MyCards"),
+    path('aboutUs', views.aboutUs, name="AboutUs"),
 ]
