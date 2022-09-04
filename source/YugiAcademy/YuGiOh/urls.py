df915985d797fedd122b26c56b1fdd946703ef7c
NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>
2022-09-04 15:44:38 -0300

    :truck: general renames

--- source/YugiAcademy/YuGiOh/urls.py
+++ source/YugiAcademy/YuGiOh/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from YuGiOh import views
+from . import views
 
 urlpatterns = [
-    path('', views.inicio, name="Inicio"),
+    path('', views.home, name="Home"),
     path('cards', views.cards, name="Cards"),
     path('decks', views.decks, name="Decks"),
-    path('myCards', views.myCards, name="MyCards"),
-    path('aboutUs', views.aboutUs, name="AboutUs"),
+    path('about', views.about, name="About"),
     path('login', views.login, name="Login"),
 ]
