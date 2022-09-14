a62e6aecdb4c3906201c68cfeb68237c5c768704
NicolasGuidoBarbeito <nbarbeito@frba.utn.edu.ar>
2022-09-14 18:47:49 -0300

    :recycle: move login and profile url to Users App

--- source/YugiAcademy/YuGiOh/urls.py
+++ source/YugiAcademy/YuGiOh/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, login, about, cards, find_or_store_spell_card, booster_packs, find_or_store_booster_pack, \
-    booster_pack_cards, find_or_store_booster_pack_card, delete_booster_pack_card, profile
+from .views import home, about, cards, find_or_store_spell_card, booster_packs, find_or_store_booster_pack, \
+    booster_pack_cards, find_or_store_booster_pack_card, delete_booster_pack_card
 
 urlpatterns = [
     path('', home, name="home"),
-    path('login', login, name="login"),
     path('about', about, name="about"),
     path('cards', cards, name="cards"),
     path('cards/registration', find_or_store_spell_card, name="find_or_store_spell_card"),
@@ -13,6 +12,5 @@ urlpatterns = [
     path('booster-pack/<int:booster_pack_id>', booster_pack_cards, name="booster_pack"),
     path('booster-pack-cards/registration', find_or_store_booster_pack_card, name="find_or_store_booster_pack_card"),
     path('booster-pack-card/delete/<int:booster_pack_card_id>', delete_booster_pack_card, name="delete_booster_pack_card"),
-    path('profile', profile, name="profile"),
 ]
 
