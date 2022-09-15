9aaf4c948358fcfaed1953ddf82044528c0b4cf6
NicolasGuidoBarbeito <nbarbeito@frba.utn.edu.ar>
2022-09-14 21:54:08 -0300

    :sparkles: fix

--- source/YugiAcademy/Users/views.py
+++ source/YugiAcademy/Users/views.py
@@ -94,7 +94,7 @@ def signup(request):
 
             if user_found_by_username is None and email_found is None:
                 user.save()
-                return redirect('YuGiOh/cards')
+                return render(request, 'YuGiOh/cards.html', user_log_in_page_context)
 
             else:
                 messages.info(request, display_message)
