166d00ea1f8be8cc46d3d8d95885d2cdcd25f47a
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-17 18:53:18 -0300

    :white_check_mark: Add test: spell card description must not be blank

--- source/YugiAcademy/YuGiOh/cards/tests/test_spell_card.py
+++ source/YugiAcademy/YuGiOh/cards/tests/test_spell_card.py
@@ -16,3 +16,10 @@ def test_spell_card_type_must_not_be_blank():
         with pytest.raises(InstanceCreationFailed) as exception_info:
             SpellCard.named(name='Pot of Greed', type=invalid_type, description='Draw 2 cards.')
         assert exception_info.message_text() == 'Type must not be blank.'
+
+
+def test_spell_card_description_must_not_be_blank():
+    for invalid_description in ['', ' ']:
+        with pytest.raises(InstanceCreationFailed) as exception_info:
+            SpellCard.named(name='Pot of Greed', type='Normal', description=invalid_description)
+        assert exception_info.message_text() == 'Description must not be blank.'
