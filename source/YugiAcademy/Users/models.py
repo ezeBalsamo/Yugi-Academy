6f6aa68d35f5a8d714c264b1ece31a962db358a7
NicolasGuidoBarbeito <nbarbeito@frba.utn.edu.ar>
2022-09-17 13:33:07 -0300

    :sparkles: upload_to added in ImageField

--- source/YugiAcademy/Users/models.py
+++ source/YugiAcademy/Users/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     password = models.CharField(max_length=25)
     description = models.CharField(max_length=250, null=True, blank=True)
     web_site = models.CharField(max_length=50, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
 
     def __str__(self):
         return f"Nombre: {self:name} - Email: {self.email}"
