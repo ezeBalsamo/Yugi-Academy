0c589c70a8da29c27bfcc561cce587181965ea54
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-25 20:24:37 -0300

    :sparkles: Add redirection from root to yugioh/

--- source/YugiAcademy/YugiAcademy/urls.py
+++ source/YugiAcademy/YugiAcademy/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('yugioh/', include('YuGiOh.urls')),
     path('accounts/', include('Accounts.urls')),
+    path('', lambda _: redirect('yugioh/')),
 ]
+
