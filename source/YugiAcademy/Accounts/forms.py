7cf9c24ce4a99c0c18db5795c595646a4d8f7d06
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-26 03:13:19 -0300

    :necktie: Add email in sign up form
    Co-authored-by: NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>

--- source/YugiAcademy/Accounts/forms.py
+++ source/YugiAcademy/Accounts/forms.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat the password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         # Remove help messages
         help_texts = {help: "" for help in fields}
 
