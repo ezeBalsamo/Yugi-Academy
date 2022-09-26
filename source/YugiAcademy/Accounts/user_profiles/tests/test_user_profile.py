3774da2717cae1323669a2e12ae9cbac8cc928c6
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-26 03:43:37 -0300

    :white_check_mark: Update user profile creation and accessing test
    Co-authored-by: NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>

--- source/YugiAcademy/Accounts/user_profiles/tests/test_user_profile.py
+++ source/YugiAcademy/Accounts/user_profiles/tests/test_user_profile.py
@@ -4,14 +4,14 @@ from Accounts.user_profiles import UserProfile
 
 
 def test_instance_creation_and_accessing():
-    user = User(username='tom', password='jerry')
+    user = User(username='tom', password='tom the cat')
     form_data = {
-        'description': 'I am the super dev',
-        'social_network_link': 'https://www.facebook.com',
-        'avatar': 'spike.jpg'
+        'description': 'I am tom',
+        'social_network_link': 'https://www.facebook.com/tom',
+        'avatar': 'tom.jpg'
     }
     user_profile = UserProfile.from_form(user=user, form_data=form_data)
-    assert user_profile.description == 'I am the super dev'
-    assert user_profile.social_network_link == 'https://www.facebook.com'
-    assert user_profile.avatar == 'spike.jpg'
+    assert user_profile.description == 'I am tom'
+    assert user_profile.social_network_link == 'https://www.facebook.com/tom'
+    assert user_profile.avatar == 'tom.jpg'
     assert str(user_profile) == 'Profile of tom'
