c7825a9a69f0f178d9bc99d682d4be871d96bd1a
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-25 22:03:47 -0300

    :sparkles: Add sign up
    Co-authored-by: NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>

--- source/YugiAcademy/Accounts/urls.py
+++ source/YugiAcademy/Accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import login_with
+from .views import login_with, signup
 
 urlpatterns = [
     path('login', login_with, name="login"),
+    path('signup', signup, name="signup"),
 ]
--- source/YugiAcademy/Accounts/views.py
+++ source/YugiAcademy/Accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 
 
@@ -18,3 +18,21 @@ def login_with(request):
         return render(request, "login.html")
 
     raise Exception(f'The {request.method} method was not expected')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Sign up has been successful")
+            return redirect('login')
+        else:
+            messages.error(request, 'Invalid credentials.')
+            return redirect('signup')
+
+    if request.method == 'GET':
+        return render(request, "signup.html")
+
+    raise Exception(f'The {request.method} method was not expected')
+
