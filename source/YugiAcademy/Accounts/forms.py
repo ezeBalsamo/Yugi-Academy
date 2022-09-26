760c7fe1cbf0c017534b11848c7586bdcfd56b18
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-26 02:46:28 -0300

    :lipstick: Add User and Profile Form
    Co-authored-by: NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>

--- source/YugiAcademy/Accounts/forms.py
+++ source/YugiAcademy/Accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, UsernameField
 from django.contrib.auth.models import User
 
 
@@ -24,3 +24,13 @@ class UpdatePasswordForm(PasswordChangeForm):
         fields = ["old_password", "new_password1", "new_password2"]
         # Remove help messages
         help_texts = {help: "" for help in fields}
+
+
+class UserAndProfileForm(forms.Form):
+    username = UsernameField(disabled=True, required=False)
+    avatar = forms.ImageField(required=False)
+    first_name = forms.CharField(label='First name', widget=forms.TextInput, required=False)
+    last_name = forms.CharField(label='Last name', widget=forms.TextInput, required=False)
+    email = forms.EmailField(required=False)
+    description = forms.CharField(label='Description', widget=forms.Textarea, required=False)
+    social_network_link = forms.URLField(label='Link to social network', required=False)
