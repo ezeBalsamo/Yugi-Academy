9c17dde20aea6495337affdf5edc68a8417f6ddb
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-25 21:33:15 -0300

    :sparkles: Add login functionality
    Co-authored-by: NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>

--- source/YugiAcademy/Accounts/urls.py
+++ source/YugiAcademy/Accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import login
+from .views import login_with
 
 urlpatterns = [
-    path('login', login, name="login"),
+    path('login', login_with, name="login"),
 ]
--- source/YugiAcademy/Accounts/views.py
+++ source/YugiAcademy/Accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
 
-def login(request):
-    return render(request, "login.html")
+def login_with(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.user_cache)
+            return render(request, 'YuGiOh/cards.html')
+        else:
+            messages.error(request, 'Invalid credentials.')
+            return redirect('login')
+
+    if request.method == 'GET':
+        return render(request, "login.html")
+
+    raise Exception(f'The {request.method} method was not expected')
