bba1576c4cbedc65cbbaf47dfd51ad5d46def492
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-25 23:46:03 -0300

    :sparkles: Add logout
    Co-authored-by: NicolasGuidoBarbeito <nicolasguidobarbeito@gmail.com>

--- source/YugiAcademy/Accounts/urls.py
+++ source/YugiAcademy/Accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import login_with, signup, profile
@@ -5,5 +6,6 @@ from .views import login_with, signup, profile
 urlpatterns = [
     path('login', login_with, name="login"),
     path('signup', signup, name="signup"),
+    path('logout', LogoutView.as_view(template_name='index.html'), name="logout"),
     path('profile', profile, name="profile"),
 ]
