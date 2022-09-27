bbaa853a6c51cf952d175437c24429ee37c36533
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-26 21:35:53 -0300

    :sparkles: Add systems when app is ready

--- source/YugiAcademy/YuGiOh/apps.py
+++ source/YugiAcademy/YuGiOh/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class YugiohConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'YuGiOh'
+    card_system = None
+    booster_pack_system = None
+
+    def ready(self):
+        from .cards import CardManagementSystem
+        from .booster_packs import BoosterPackManagementSystem
+        self.card_system = CardManagementSystem()
+        self.booster_pack_system = BoosterPackManagementSystem()
