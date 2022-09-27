2d96f9a8b76bc2dde14e9efed3955c679705b314
NicolasGuidoBarbeito <nbarbeito@frba.utn.edu.ar>
2022-09-27 15:23:55 -0300

    :fire: Remove unused code

--- source/YugiAcademy/Messages/messages/message_management_system.py
+++ source/YugiAcademy/Messages/messages/message_management_system.py
@@ -6,8 +6,5 @@ class MessageManagementSystem:
     def __init__(self):
         self.messages_repository = Message.objects
 
-    def messages(self):
-        return list(self.messages_repository.all())
-
     def send_message(self, message):
         message.save()
