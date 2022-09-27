f54e5b00869c171ef38799cbf80d5c09159d4dfa
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-09-27 02:45:26 -0300

    :art: handle errors in spell card registration view

--- source/YugiAcademy/YuGiOh/cards/views/spell_card_view.py
+++ source/YugiAcademy/YuGiOh/cards/views/spell_card_view.py
@@ -4,15 +4,20 @@ from django.shortcuts import redirect, render
 from YuGiOh.cards.forms import SpellCardForm
 from YuGiOh.cards import SpellCard
 from YuGiOh.models import card_system
+from assertions import InstanceCreationFailed, SystemRestrictionInfringed
 
 
 def find_or_store_spell_card(request):
     if request.method == 'POST':
         form = SpellCardForm(request.POST)
         if form.is_valid():
-            spell_card = SpellCard.from_from(form.cleaned_data)
-            card_system().store_spell_card(spell_card)
-            return redirect('cards')
+            try:
+                spell_card = SpellCard.from_from(form.cleaned_data)
+                card_system().store_spell_card(spell_card)
+                return redirect('cards')
+            except (InstanceCreationFailed, SystemRestrictionInfringed) as error:
+                messages.error(request, str(error))
+                return render(request, 'YuGiOh/spell_card_registration.html', {form: SpellCardForm()})
 
     if request.method == 'GET':
         context = {
