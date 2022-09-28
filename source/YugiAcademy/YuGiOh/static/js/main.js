7e6d32f5333276e7ef54a5a95cacb3cab23efeeb
NicolasGuidoBarbeito <nbarbeito@frba.utn.edu.ar>
2022-09-28 19:28:19 -0300

    :art: Formatting by Bálsamo Shortcut CONTROL+ALT+SHIFT+L.

--- source/YugiAcademy/YuGiOh/static/js/main.js
+++ source/YugiAcademy/YuGiOh/static/js/main.js
@@ -1,83 +1,77 @@
-
 window.addEventListener("load", getMessageInformation);
 
-function getMessageInformation(){
-    let messages_error = document.getElementsByClassName("message-error");
+function getMessageInformation() {
+  let messages_error = document.getElementsByClassName("message-error");
 
-    let messages_ok = document.getElementsByClassName("message-info");
+  let messages_ok = document.getElementsByClassName("message-info");
 
+  if (messages_error[0] != null) {
+    let message_error = messages_error[0].textContent;
 
-if (messages_error[0] != null){
-
-    let message_error =  messages_error[0].textContent
-
-    if(message_error === 'Invalid credentials.'){
-        Swal.fire({
-        imageUrl: '/static/assets/invalid-login.jpeg',
+    if (message_error === "Invalid credentials.") {
+      Swal.fire({
+        imageUrl: "/static/assets/invalid-login.jpeg",
         imageWidth: 500,
         imageHeight: 800,
-        timer: 4000
-    })
+        timer: 4000,
+      });
     }
 
-    if(message_error === 'Invalid sign up credentials.'){
-        Swal.fire({
-        imageUrl: '/static/assets/invalid-sign-up.jpeg',
-        imageWidth: 500,
-        imageHeight: 800
-    })
-    }
-
-    if(message_error === 'Profile update has failed.'){
-        Swal.fire({
-        imageUrl: '/static/assets/invalid-profile-update.jpeg',
+    if (message_error === "Invalid sign up credentials.") {
+      Swal.fire({
+        imageUrl: "/static/assets/invalid-sign-up.jpeg",
         imageWidth: 500,
         imageHeight: 800,
-        timer: 12000
-    })
+      });
     }
 
-    if(message_error === 'Password update has failed.'){
-        Swal.fire({
-        imageUrl: '/static/assets/invalid-password-update.jpeg',
+    if (message_error === "Profile update has failed.") {
+      Swal.fire({
+        imageUrl: "/static/assets/invalid-profile-update.jpeg",
         imageWidth: 500,
         imageHeight: 800,
-        timer: 12000
-    })
+        timer: 12000,
+      });
     }
+
+    if (message_error === "Password update has failed.") {
+      Swal.fire({
+        imageUrl: "/static/assets/invalid-password-update.jpeg",
+        imageWidth: 500,
+        imageHeight: 800,
+        timer: 12000,
+      });
+    }
+  }
+
+  if (messages_ok[0] != null) {
+    let message_ok = messages_ok[0].textContent;
+
+    if (message_ok === "Sign up has been successful") {
+      Swal.fire({
+        imageUrl: "/static/assets/successful-signup.jpeg",
+        imageWidth: 500,
+        imageHeight: 800,
+        timer: 4000,
+      });
+    }
+
+    if (message_ok === "Profile has been successfully updated") {
+      Swal.fire({
+        imageUrl: "/static/assets/successful-profile-update.jpeg",
+        imageWidth: 500,
+        imageHeight: 800,
+        timer: 4000,
+      });
+    }
+
+    if (message_ok === "Password has been successfully updated") {
+      Swal.fire({
+        imageUrl: "/static/assets/successful-password-update.jpeg",
+        imageWidth: 500,
+        imageHeight: 800,
+        timer: 4000,
+      });
+    }
+  }
 }
-
-if (messages_ok[0] != null){
-    let message_ok = messages_ok[0].textContent
-
-    if(message_ok === 'Sign up has been successful'){
-        Swal.fire({
-        imageUrl: '/static/assets/successful-signup.jpeg',
-        imageWidth: 500,
-        imageHeight: 800,
-        timer: 4000
-    })
-    }
-
-    if(message_ok === 'Profile has been successfully updated'){
-        Swal.fire({
-        imageUrl: '/static/assets/successful-profile-update.jpeg',
-        imageWidth: 500,
-        imageHeight: 800,
-        timer: 4000
-    })
-    }
-
-    if(message_ok === 'Password has been successfully updated'){
-        Swal.fire({
-        imageUrl: '/static/assets/successful-password-update.jpeg',
-        imageWidth: 500,
-        imageHeight: 800,
-        timer: 4000
-    })
-    }
-
-
-}
-}
-
