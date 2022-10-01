c55c4e6694f5d8123e49ec03c28337a1d90d9b8d
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-10-01 15:06:49 -0300

    :card_file_box: Add image to trap card

--- source/YugiAcademy/YuGiOh/cards/trap_card.py
+++ source/YugiAcademy/YuGiOh/cards/trap_card.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.db.models.fields.files import ImageFieldFile
 
 from .card import Card
 from YuGiOh.booster_packs import BoosterPackCard
@@ -10,14 +11,15 @@ class TrapCard(Card):
     type_description = 'trap card'
     type = models.CharField(max_length=20)
     set = GenericRelation(to=BoosterPackCard, related_query_name='trap_card')
+    image = models.ImageField(upload_to='cards/trap_cards', default='cards/card-back.jpg')
 
     @classmethod
-    def named(cls, name: str, type: str, description: str):
+    def named(cls, name: str, type: str, description: str, image: ImageFieldFile):
         enforce_not_blank(name, "Name")
         enforce_not_blank(type, "Type")
         enforce_not_blank(description, "Description")
 
-        return cls(name=name, type=type, description=description)
+        return cls(name=name, type=type, description=description, image=image)
 
     def synchronize_with(self, trap_card):
         self.name = trap_card.name
