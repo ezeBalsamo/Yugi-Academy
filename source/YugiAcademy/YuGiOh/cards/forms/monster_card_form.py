48e390a44820662c2d1d25a2e6b1d6664484c44d
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-10-01 18:04:37 -0300

    :bug: Update monster card form level min value

--- source/YugiAcademy/YuGiOh/cards/forms/monster_card_form.py
+++ source/YugiAcademy/YuGiOh/cards/forms/monster_card_form.py
@@ -7,7 +7,7 @@ class MonsterCardForm(forms.Form):
     name = forms.CharField(label_suffix='', max_length=50)
     race = forms.CharField(label_suffix='', max_length=25)
     attribute = forms.ChoiceField(label_suffix='', choices=MonsterCard.Attributes.choices)
-    level = forms.IntegerField(label_suffix='', min_value=0, max_value=12)
+    level = forms.IntegerField(label_suffix='', min_value=1, max_value=12)
     attack = forms.IntegerField(label_suffix='', min_value=0)
     defense = forms.IntegerField(label_suffix='', min_value=0)
     image = forms.ImageField(label_suffix='', required=False)
