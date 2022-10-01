5ab519220e8ce86c42b45334e5ecc889be151de2
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-10-01 17:50:29 -0300

    :construction: Add monster card form

---
+++ source/YugiAcademy/YuGiOh/cards/forms/monster_card_form.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from YuGiOh.cards import MonsterCard
+
+
+class MonsterCardForm(forms.Form):
+    name = forms.CharField(label_suffix='', max_length=50)
+    race = forms.CharField(label_suffix='', max_length=25)
+    attribute = forms.ChoiceField(label_suffix='', choices=MonsterCard.Attributes.choices)
+    level = forms.IntegerField(label_suffix='', min_value=0, max_value=12)
+    attack = forms.IntegerField(label_suffix='', min_value=0)
+    defense = forms.IntegerField(label_suffix='', min_value=0)
+    image = forms.ImageField(label_suffix='', required=False)
+    description = forms.CharField(label_suffix='', max_length=8000, widget=forms.Textarea)
