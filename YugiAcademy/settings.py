7b5a20f8425c8620e1decfc757512d5705050e17
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-10-02 05:18:08 -0300

    :closed_lock_with_key: Update secret key access

--- YugiAcademy/settings.py
+++ YugiAcademy/settings.py
@@ -27,7 +27,10 @@ IS_HEROKU = "DYNO" in os.environ
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU:
